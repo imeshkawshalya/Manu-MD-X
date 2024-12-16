df5a5f3801761851d22a17e735d835a4231c4968
Kavi-md <155098034+imeshkawshalya@users.noreply.github.com>
2024-12-16 13:59:59 +0530

    Update session_id.js

--- session_id.js
+++ session_id.js
@@ -43,5 +43,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&aMM1QajT#o5Swildp2x9t5WPaJx_5i-_RQzZJKO3h5nFVhFK3EKY",
 };
